625ef34cf1cef24873b55f0f4d8a5cff339a0919
Olusheyi Gaji <seezie@gmail.com>
2022-06-18 14:05:46 -0400

    corrected the author variable in my models

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,10 +12,14 @@ from django.contrib.auth import get_user_model
 #Author : A Foreign Key to the current user model. Make use of Django’s get_user_model function.
 #Created_date : A date-time column, use Django’s models.DateTimeField. 
 #Published_date : A date-time column, use Django’s models.DateTimeField. 
+
 user = get_user_model()
 class Post(models.Model):
     tittle = models.CharField(max_length=200)
     text = models.TextField()
-    author = models.ForeignKey()
+    author = models.ForeignKey(user, on_delete= models.CASCADE)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.tittle
